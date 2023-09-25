635446b3d71e4f4e468529382434d25076e95906
Markus23D <maan0007@stud.kea.dk>
2023-09-25 14:33:30 +0200

    Rooms og get/set metoder

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -1,2 +1,37 @@
 public class Adventure {
+   private Room currentRoom;
+
+   public Adventure() {
+       Room room1 = new Room("Room 1", "room with no distinct features, except two doors");
+       Room room2 = new Room("Room 2", "");
+       Room room3 = new Room("Room 3", "");
+       Room room4 = new Room("Room 4", "");
+       Room room5 = new Room("Room 5", "");
+       Room room6 = new Room("Room 6", "");
+       Room room7 = new Room("Room 7", "");
+       Room room8 = new Room("Room 8", "");
+       Room room9 = new Room("Room 9", "");
+
+       room1.setEast(room4);
+       room1.setSouth(room7);
+       room2.setEast(room3);
+       room2.setSouth(room5);
+       room3.setWest(room2);
+       room4.setEast(room5);
+       room4.setSouth(room1);
+       room5.setWest(room4);
+       room5.setNorth(room2);
+       room6.setEast(room5);
+       room6.setSouth(room9);
+       room7.setNorth(room1);
+       room7.setEast(room8);
+       room8.setWest(room7);
+       room9.setNorth(room6);
+
+
+       currentRoom = room1;
+
+   }
+
+
 }
--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,3 +1,59 @@
 public class Room {
+private String name;
+private String description;
+private Room north;
+private Room east;
+private Room south;
+private Room west;
+
+
+public Room(String name, String description) {
+    this.name = name;
+    this.description = description;
+}
+
+public String getName(){
+    return name;
+}
+
+public String getDescription(){
+    return description;
+}
+
+public Room getNorth(){
+    return north;
+}
+
+public Room getEast(){
+    return east;
+}
+
+public Room getSouth() {
+    return south;
+}
+
+public Room getWest() {
+    return west;
+}
+
+public void setNorth(Room north){
+    this.north = north;
+}
+
+public void setEast(Room east){
+    this.east = east;
+}
+
+public void setSouth(Room south){
+    this.south = south;
+}
+
+public void setWest(Room west){
+    this.west = west;
+}
+
+
+
+
 
 }
